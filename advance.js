69cd04ef94f87dbdc29a32e8ce9c66163fb18477
advance-design <gilus@nutpa.net>
2017-02-15 08:23:01 -0600

    Add files via upload

---
+++ advance.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('#maincontent').removeClass('hidden');
+    $('#maincontent').fadeOut(0.1);
+    $('#maincontent').fadeIn(1000);
+    $('#simple-branding').removeClass('hidden');
+    $('#simple-branding').fadeOut(0.1);
+    $('#simple-branding').fadeIn(4000);
+
+
+});
+
+$(document).ready(function() {
+
+	$('#advance-logo').removeClass('hidden').delay(800);
+    $('#advance-logo').fadeOut(0.1);
+    $('#advance-logo').fadeIn(2000);
+    $('#simple-branding-sub').removeClass('hidden').delay(800);
+    $('#simple-branding-sub').fadeOut(0.1);
+    $('#simple-branding-sub').fadeIn(5000);
+})
